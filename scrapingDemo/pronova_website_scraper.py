13460703739a1b1873756abe981af6d15bcd6c73
Iris Yang <irisyang1010@gmail.com>
2025-03-02 02:44:39 -0500

    started building

---
+++ scrapingDemo/pronova_website_scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import json
+import os
+
+def extract_text_from_url(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    
